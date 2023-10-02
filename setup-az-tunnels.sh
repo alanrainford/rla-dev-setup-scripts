d3621ec5999bbc1c2c9dcc440ff887650c501ed5
Rainford <laura.rainford@igt.com>
2023-10-02 14:03:34 -0500

    Fix bracket

--- setup-az-tunnels.sh
+++ setup-az-tunnels.sh
@@ -239,7 +239,8 @@ function installChrome()
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     sudo apt install --fix-broken -y
-    sudo dpkg -i google-chrome-stable_current_amd64.deb}
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+}
 
 done="false"
 while [ "$done" != "true" ]
