bc6ad234e8ca6f08046ef91fa00101222fcb6a2b
Alan Rainford <alan.rainford@igt.com>
2023-10-02 15:08:52 -0500

    Install VS Code

--- setup-az-tunnels.sh
+++ setup-az-tunnels.sh
@@ -290,4 +290,37 @@ do
 done
 
 #################################################################################
+function installVSCode()
+{
+    sudo apt-get install -y wget gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
+
+    sudo apt install -y apt-transport-https
+    sudo apt update -y
+    sudo apt install -y code # or code-insiders
+
+}
+
+done="false"
+while [ "$done" != "true" ]
+do
+    read -p $'\e[7mWould you like to install VSCode[y/n]? \e[0m' userInput
+
+    if [[ -z "$userInput" ]]; then
+        printf '%s\n' "No input entered"
+        exit 1
+    fi
+
+    case $userInput in
+        [Yy]* ) installVSCode; break;;
+        [Nn]* ) done="true";;
+        * ) echo "Please answer y or n";;
+    esac
+done
+
+
+#################################################################################
 echo "All done!"
