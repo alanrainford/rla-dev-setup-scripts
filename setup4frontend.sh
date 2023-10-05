0c1c0bf0b56d81a9cbddc0c4749d46846fc1ed92
GearHead <99506724+alanrainford@users.noreply.github.com>
2023-10-05 16:45:31 -0500

    Update setup4frontend.sh

--- setup4frontend.sh
+++ setup4frontend.sh
@@ -36,11 +36,12 @@ fi
 
 #################################################################################
 echo "TODO: add frontend apps here!!"
-sudo apt install -y git npm angular nodejs
+sudo apt install -y git npm nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 source ~/.bashrc
 nvm install latest
+sudo npm install -g @angular/cli
 #################################################################################
 echo "All done!"
